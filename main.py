a8be5ef099f6bee628aa52378c4695e114f662f2
AmmarAlani <60627873+AmmarAlani@users.noreply.github.com>
2021-01-30 17:05:51 -0600

    pdf parser implemented: reading multiple pages

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from flask import Flask
 from flask import render_template, request, url_for, redirect, g, session, flash
+import PyPDF2
+
+
+
+def fileReader():
+    pdfName = "file.pdf"
+
+    pdfRead = PyPDF2.PdfFileReader(pdfName)
+
+    for i in range(pdfRead.getNumPages()):
+        page = pdfRead.getPage(i)
+        print("Page No: " + str(1 + pdfRead.getPageNumber(page)))
+        pageContent = page.extractText()
+        print(pageContent)
+        
 
 app = Flask(__name__)
 app.secret_key = "123"
