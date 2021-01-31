f2e14b7c6244a8ae3f8f441a469ccf5ad1b984b1
AmmarAlani <60627873+AmmarAlani@users.noreply.github.com>
2021-01-30 22:37:48 -0600

    table function included

--- extraction/sample.py
+++ extraction/sample.py
@@ -1,13 +1,13 @@
 import PyPDF2
-from tabula import read_pdf
+import tabula as tb
 from tabulate import tabulate
 
 
 def fileReader():
-    pdfName = open(
-        'C:/Users/ammar/Desktop/tamuHack/TAMU-Hack/extraction/file.pdf', 'rb')
+    pdfName = open('extraction/file.pdf', 'rb')
+
     try:
-        pdfRead = PyPDF2.PdfFileReader(pdfNam)
+        pdfRead = PyPDF2.PdfFileReader(pdfName)
 
         for i in range(pdfRead.getNumPages()):
             page = pdfRead.getPage(i)
@@ -20,5 +20,23 @@ def fileReader():
         print("Error {}".format(e))
 
 
+def tableReader():
+    pdfName = "extraction/file3.pdf"
+
+    try:
+        dfs = tb.read_pdf(pdfName, pages='all')
+        print(dfs)
+    except Exception as e:
+        print("Error {}".format(e))
+
+
+'''
+    try:
+        df = tb(pdfName)
+        print(df)
+    except Exception as e:
+        print("Error {}".format(e))
+'''
+
 if __name__ == "__main__":
-    fileReader()
+    tableReader()
