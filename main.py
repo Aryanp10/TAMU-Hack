0e29a7d0c427eb79ff79404ec2e14ad648dc9812
Aryanp10 <aaryanparajulee@gmail.com>
2021-01-30 19:51:17 -0600

    main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template, request, url_for, redirect, g, session, flash
 import PyPDF2
 from flask_sqlalchemy import SQLAlchemy
-
+import sqlite3
 
 # def fileReader():
 #     pdfName = "old/file.pdf"
@@ -18,31 +18,41 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "123"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db=SQLAlchemy(app)
+# db=SQLAlchemy(app)
 
-class Info(db.Model):
-    id=db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(200), nullable = False)
-    school= db.Column(db.String(200), nullable = False)
-    email = db.Column(db.String(100), nullable = False)
-    password = db.Column(db.String(200), nullable = False)
+# class user_info(db.Model):
+#     id=db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(200), nullable = False)
+#     school= db.Column(db.String(200), nullable = False)
+#     email = db.Column(db.String(100), nullable = False)
+#     password = db.Column(db.String(200), nullable = False)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+
+
+conn = sqlite3.connect('database.db')
 
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
+
         session['name'] = request.form['name']
         session['school'] = request.form['school']
         session['email'] = request.form['email']
         session['password'] = request.form['password']
-        print(session)
+        
+        # user = user_info(id=id_no, name = session['name'], school = session['school'], email=session['email'], password=session['password'])
+        # db.session.add(user)
+        # db.session.commit()
+        conn.execute("CREATE TABLE t1 ( x int);")
+        conn.execute("INSERT INTO t1 VALUES (1);")
+        # conn.execute("SELECT * FROM t1;")
         return redirect(url_for("signin"))
-    else: 
+    else:
         return render_template("index.html")
 
 @app.route("/signin", methods=["POST", "GET"])
