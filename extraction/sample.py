c4f373adb36f34c5972f9cb0923552748d119378
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2021-01-30 21:40:39 -0600

    File extraction works

--- extraction/sample.py
+++ extraction/sample.py
@@ -1,12 +1,21 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-def fileReader():
-    file_path = "extraction/file.pdf"
-    pdfFileObj = open(file_path)
-    pdfReader = PdfFileReader(pdfFileObj)
 
-    print(pdfReader)
+def fileReader2():
+    pdfName = open("extraction/file.pdf", 'rb')
+    try:
+        pdfRead = PdfFileReader(pdfName)
+        for i in range(pdfRead.getNumPages()):
+            page = pdfRead.getPage(i)
+            if i > 0:
+                print(
+                    '==================================================================================')
+            pageContent = page.extractText()
+            print(pageContent)
+    except Exception as e:
+        print("Error {}".format(e))
+
 
 if __name__ == "__main__":
-    fileReader()
+    fileReader2()
     print("Hello")
