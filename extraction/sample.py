2139a11d3a8592562bb8e6483adc6b6bfc3f5bce
AmmarAlani <60627873+AmmarAlani@users.noreply.github.com>
2021-01-31 03:26:57 -0600

    parser updated

--- extraction/sample.py
+++ extraction/sample.py
@@ -1,4 +1,8 @@
 import tabula as tb
+import camelot
+import pandas as pd
+import numpy as np
+import re
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 # Read all the text from a pdf file and save teh content on
@@ -10,29 +14,53 @@ def fileReader2(pagelist):
         pdfRead = PdfFileReader(pdfName)
         for i in range(pdfRead.getNumPages()):
             page = pdfRead.getPage(i)
-            pagelist.append(page.extractText().replace('\n', ''))
+            pagelist.append(page.extractText())
+            # page.extractText()
     except Exception as e:
         print("Error {}".format(e))
 
 # Extract Important information ("Course info", "Meeting time", etc.)
 
 
-def extractInfo(pagelist):
-    print("Hello")
+# def extractInfo(pagelist):
+#     print("Hello")
 
 
-def tableReader():
-    pdfName = "extraction/file3.pdf"
+# def camReader():
+#     tables = camelot.read_pdf("extraction/file2.pdf")
+#     print(tables)
 
-    try:
-        dfs = tb.read_pdf(pdfName, pages='all')
-        print(dfs)
-    except Exception as e:
-        print("Error {}".format(e))
+
+# def tabReader():
+#     tb.io.read_pdf_with_template(
+#         input_path="extraction/file2.pdf", stream=True)
+
+
+# def tableReader():
+#     pdfName = "extraction/file3.pdf"
+
+#     try:
+
+#         dfs = tb.read_pdf(pdfName, pages='all', multiple_tables=True)
+#         print(dfs[0][0][0])
+#     except Exception as e:
+#         print("Error {}".format(e))
 
 
 if __name__ == "__main__":
-
     pagelist = []
     fileReader2(pagelist)
-    print(pagelist)
+    print(pagelist[0])
+    # counter = 0
+    for i in pagelist:
+        for f in i:
+            if f == ' ' or f == '\n':
+                pass
+
+        print(i)
+    #     for f in i:
+    #         if f ==
+    #         print(counter)
+    #         counter = counter + 1
+    # print(counter)
+    # counter = counter + 1
