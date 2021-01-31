0e77282dfae871f588c89afa8c7616b328a20dfa
AmmarAlani <60627873+AmmarAlani@users.noreply.github.com>
2021-01-30 21:35:51 -0600

    python file updated: parser reading

--- extraction/sample.py
+++ extraction/sample.py
@@ -6,16 +6,18 @@ from tabulate import tabulate
 def fileReader():
     pdfName = open(
         'C:/Users/ammar/Desktop/tamuHack/TAMU-Hack/extraction/file.pdf', 'rb')
+    try:
+        pdfRead = PyPDF2.PdfFileReader(pdfNam)
 
-    pdfRead = PyPDF2.PdfFileReader(pdfName)
-
-    for i in range(pdfRead.getNumPages()):
-        page = pdfRead.getPage(i)
-        if i > 0:
-            print(
-                '==================================================================================')
-        pageContent = page.extractText()
-        print(pageContent)
+        for i in range(pdfRead.getNumPages()):
+            page = pdfRead.getPage(i)
+            if i > 0:
+                print(
+                    '==================================================================================')
+            pageContent = page.extractText()
+            print(pageContent)
+    except Exception as e:
+        print("Error {}".format(e))
 
 
 if __name__ == "__main__":
