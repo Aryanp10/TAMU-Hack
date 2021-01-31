d5e145cb5969c417fc892418dbe101c7bd102b2f
Aryanp10 <aaryanparajulee@gmail.com>
2021-01-30 18:43:29 -0600

    main

--- main.py
+++ main.py
@@ -21,9 +21,19 @@ app.secret_key = "123"
 
 @app.route("/", methods=["POST", "GET"])
 def index():
+    if request.method == "POST":
+        session['name'] = request.form['name']
+        session['school'] = request.form['school']
+        session['email'] = request.form['email']
+        session['password'] = request.form['password']
+        print(session)
+        return redirect(url_for("signin"))
+    else: 
+        return render_template("index.html")
 
-    return render_template("index.html")
+@app.route("/signin", methods=["POST", "GET"])
+def signin():
+    return render_template("signin.html")
 
 if __name__ == "__main__":
-    fileReader()
     app.run(debug=True)
