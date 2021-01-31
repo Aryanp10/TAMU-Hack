725fff1f4ffc45bc1758227eb429b8f13be4e337
Aryanp10 <aaryanparajulee@gmail.com>
2021-01-30 20:48:49 -0600

    MAIN

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import render_template, request, url_for, redirect, g, session, flash
 import PyPDF2
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+import random, string
+from decimal import Decimal
 
 # def fileReader():
 #     pdfName = "old/file.pdf"
@@ -33,30 +35,56 @@ app.secret_key = "123"
 #         return '<Task %r>' % self.id
 
 
-conn = sqlite3.connect('database.db')
+def genID(length):
+    id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(length)])
+    return id
+
 
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        
+        # command = "DROP TABLE user_info"
+        # cursor.execute(command)
+        # command = "CREATE TABLE user_info (id varchar(5), name varchar(30), school varchar(100), email varchar(40), password varchar(100) )"
+        # cursor.execute(command)
 
+        cursor.execute("SELECT * FROM user_info")       
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+
+        session['id'] = genID(5)
         session['name'] = request.form['name']
         session['school'] = request.form['school']
         session['email'] = request.form['email']
         session['password'] = request.form['password']
-        
-        # user = user_info(id=id_no, name = session['name'], school = session['school'], email=session['email'], password=session['password'])
-        # db.session.add(user)
-        # db.session.commit()
-        conn.execute("CREATE TABLE t1 ( x int);")
-        conn.execute("INSERT INTO t1 VALUES (1);")
-        # conn.execute("SELECT * FROM t1;")
+ 
+        command = f"INSERT INTO user_info VALUES ('{session['id']}', '{session['name']}', '{session['school']}', '{session['email']}', '{session['password']}')"
+        cursor.execute(command)
+
+        cursor.execute("SELECT * FROM user_info")       
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+
+        conn.commit()
         return redirect(url_for("signin"))
     else:
         return render_template("index.html")
 
 @app.route("/signin", methods=["POST", "GET"])
 def signin():
+
+
+    # session['signin_email'] = request.form['signin_email']
+    # session['signin_password'] = request.form['signin_password']
+
+    # command=f"SELECT COUNT(*) FROM user_info WHERE email LIKE '%{session}%'"
+
     return render_template("signin.html")
 
 if __name__ == "__main__":
